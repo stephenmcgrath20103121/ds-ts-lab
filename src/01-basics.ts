0c93023c3616b53a1a240770e74d8847347e412a
stephenmcgrath20103121 <20103121@setu.ie>
2025-09-25 08:22:17 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -6,6 +6,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+  name: string;
+  department: string;
+  contact: {
+    email: string;
+    extension: number
+  }
+}
+
 const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
@@ -22,7 +31,7 @@ const friends = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -31,7 +40,7 @@ const colleague1 = {
   },
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -40,7 +49,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
