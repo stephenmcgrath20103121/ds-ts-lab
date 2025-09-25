29817f6220994c0d5f05fb9ff6b76b8596868a5d
stephenmcgrath20103121 <20103121@setu.ie>
2025-09-25 08:24:02 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -15,6 +15,11 @@ interface Colleague {
   }
 }
 
+interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
+
 const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
@@ -57,7 +62,7 @@ const colleague3: Colleague = {
     extension: 125,
   },
 };
-const colleagues = {
+export const colleagues : ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
